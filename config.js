6b3995bc777497b130b58128ce1f5783bbea6657
Alex Moreno <alexmorenocano@gmail.com>
2017-05-10 12:18:30 +0800

    added debug and removed the logging library

--- config.js
+++ config.js
@@ -7,28 +7,8 @@ config.server = {
     lifetimeCheckInterval: 1000,        // Minimum interval between lifetime checks in ms
     udpWindow: 100,
     defaultType: 'Device',
-    logLevel: 'FATAL',
     ipProtocol: 'udp4',
-    serverProtocol: 'udp4',
-    formats: [
-        {
-            name: 'application-vnd-oma-lwm2m/text',
-            value: 1541
-        },
-        {
-            name: 'application-vnd-oma-lwm2m/tlv',
-            value: 1542
-        },
-        {
-            name: 'application-vnd-oma-lwm2m/json',
-            value: 1543
-        },
-        {
-            name: 'application-vnd-oma-lwm2m/opaque',
-            value: 1544
-        }
-    ],
-    writeFormat: 'application-vnd-oma-lwm2m/text'
+    serverProtocol: 'udp4'
 };
 
 // Configuration of the LWTM2M Client
@@ -36,19 +16,11 @@ config.server = {
 config.client = {
     lifetime: '85671',
     version: '1.0',
-    logLevel: 'DEBUG',
     observe: {
         period: 3000
     },
     ipProtocol: 'udp4',
-    serverProtocol: 'udp4',
-    formats: [
-        {
-            name: 'lightweightm2m/text',
-            value: 1541
-        }
-    ],
-    writeFormat: 'lightweightm2m/text'
+    serverProtocol: 'udp4'
 };
 
 module.exports = config;
